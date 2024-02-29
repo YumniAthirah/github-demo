2750ed41b7dbae4c83a8c0a55dc320775ef27154
Firhish <86069053+Firhish@users.noreply.github.com>
2024-03-01 00:42:55 +0800

    Commit baru

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Worlddduwaaaaa!");
         System.out.println("hi");
+        System.out.println("dari daus");
     }
 }
